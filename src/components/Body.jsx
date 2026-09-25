d84bcda4e6dfbdd2615768cc24e44fca0fd823b5
Gideon Tetteh Nyamedi <nyamedigideon1@gmail.com>
2026-09-25 23:15:23 +0000

    bored api is not working

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const Body = () => {
-  return (
-    <div className='bg-red-500 text-6xl' >Body</div>
-  )
-}
+  const [activity, setActivity] = useState(null);
 
-export default Body
+  useEffect(() => {
+    axios
+      .get('https://bored-api.appbrewery.com/random')
+      .then(response => {
+        setActivity(response.data);
+      });
+  }, []);
+
+  return (
+    <div className="bg-red-500">
+      <h1>The header</h1>
+
+      <p>{activity?.activity}</p>
+      <p>Type: {activity?.type}</p>
+      <p>Participants: {activity?.participants}</p>
+    </div>
+  );
+};
+
+export default Body;
